43df7e4e5255164f09f06e04af5ac2cb1ca765d7
niharikamutha <0483niharika@gmail.com>
2025-10-10 14:18:18 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<ctype.h>
+#define SIZE 20
 
-int GCD(a,b)
+struct stack
 {
-    if(a==0)
-        return b;
-    return (b%a,a);
+    int top ;
+    char data[SIZE];
+
+};
+typedef struct stack STACK;
+
+void push(STACK *s ,char item)
+{
+s->data[++(s->top)]=item;
+}
+
+char pop(STACK *s)
+{
+    return s->data[(s->top)--];
+}
+
+int preced(char symbol)
+{
+    switch(symbol)
+    {
+        case '+':return 1;
+        case '-':return 1;
+        case '*':return 3;
+        case '/':return 3;
+        case '^':return 5;
+    }
+}
+
+void infixtopostfix(STACK *s , char infix[20])
+{
+    char symbol , postfix[20] , temp;
+    int i , j=0 ;
+    for (i = 0 ; infix[i]!='\0'; i++)
+    {
+        symbol = infix[i];
+        if(isalnum(symbol))
+            postfix[j++]=symbol;
+        else
+        {
+            switch(symbol)
+            {
+                case '{':push(s,symbol);
+                case '}':temp = pop(s);
+                         while(temp!='(')
+                         {
+                             postfix[j++]=temp;
+                             temp = pop(s);
+                         }break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                    if(s->top==-1||s->data[s->top]=='(')
+                            push(s , symbol);
+                        else
+                        {
+                            while(preced(s->data[s->top])>=preced(symbol)&&s->top!=-1&&s->data[s->top]!='(')
+                            {
+                                postfix[j++]=pop(s);
+                            }
+                        push(s,symbol);
+                        }
+                    break;
+            }
+        }
+    }
+    while(s->top!=-1)
+    {
+        postfix[j++]=pop(s);
+    }
+    postfix[j]='\0';
+    printf("\n postfix expression is %s",postfix);
 }
 
 int main()
 {
-    int a, b;
-    printf("enter two values : \n");
-    scanf("%d%d",&a,&b);
-    printf("GCD value for %d and %d is %d \n",a,b,GCD(a,b));
+    char infix[20];
+    STACK s;
+    s.top =-1;
+    printf("\n read infix expression:\n");
+    scanf("%s",infix);
+    infixtopostfix(&s,infix);
     return 0;
-
 }
